4aa55602a5f1e1883f9ad50ae6cc504c14686b06
roiamiel1 <roiamiel.ra@gmail.com>
2020-01-27 23:23:25 +0200

    partB

--- src/main/java/Pong.java
+++ src/main/java/Pong.java
@@ -1,8 +1,10 @@
-import engine.Ball;
-import engine.GameEngine;
-import engine.Greta;
+import engine.*;
+import io.reactivex.Observable;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+import static io.reactivex.Observable.combineLatest;
 
 public class Pong  {
     private static final int WIDTH = 500;
@@ -12,12 +14,35 @@ public class Pong  {
     private final Greta greta = Greta.create(WIDTH, HEIGHT);
     private final GameEngine gameEngine = GameEngine.create(WIDTH, HEIGHT);
 
+    private Point playerLocation = new Point(0, 0);
+
     public void init() {
-        // todo you code goes here
-        gameEngine.getRenderThread().scheduleDirect(() -> {
-           gameEngine.drawFrame(new Point(0, 0),
-                   new Point(0, 0), new Point(0, 0));
-        });
+        Observable.combineLatest((gameEngine.getClicks()
+                .sample(6, TimeUnit.MILLISECONDS)
+                .map(event -> {
+                    int yDirection = 0;
+                    if (event == Keys.UP) {
+                        yDirection = -1;
+                    } else if (event == Keys.DOWN) {
+                        yDirection = 1;
+                    }
+
+                    playerLocation.y += yDirection;
+
+                    if (playerLocation.y < 0) {
+                        playerLocation.y = 0;
+                    } else if (playerLocation.y > HEIGHT - Greta.playerHeight) {
+                        playerLocation.y = HEIGHT - Greta.playerHeight;
+                    }
+
+                    return playerLocation;
+                })), greta.getLocation(), ball.getLocation(), (playerLocation, gretaLocation, ballLocation) -> {
+            greta.setBallLocation(ballLocation);
+            ball.setPlayersLocation(playerLocation, gretaLocation);
+            return new Triple<>(playerLocation, gretaLocation, ballLocation);
+        }).throttleFirst(35, TimeUnit.MILLISECONDS)
+                .observeOn(gameEngine.getRenderThread())
+                .subscribe(t -> gameEngine.drawFrame(t.getFirst(), t.getSecond(), t.getThird()));
     }
 
     public static void main(String[] args) {
