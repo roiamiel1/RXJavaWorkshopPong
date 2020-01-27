a18416aaeb831573f8e72a2f92c2402c98f2bad6
roiamiel1 <roiamiel.ra@gmail.com>
2020-01-27 23:02:15 +0200

    PartA

--- src/main/java/Pong.java
+++ src/main/java/Pong.java
@@ -1,6 +1,11 @@
 import engine.Ball;
 import engine.GameEngine;
 import engine.Greta;
+import engine.Keys;
+import io.reactivex.Observable;
+
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class Pong  {
     private static final int WIDTH = 500;
@@ -10,8 +15,32 @@ public class Pong  {
     private final Greta greta = Greta.create(WIDTH, HEIGHT);
     private final GameEngine gameEngine = GameEngine.create(WIDTH, HEIGHT);
 
+    private Point playerLocation = new Point(0, 0);
+
     public void init() {
-        // todo you code goes here
+        gameEngine.getClicks()
+                .sample(6, TimeUnit.MILLISECONDS)
+                .map(event -> {
+                    int yDirection = 0;
+                    if (event == Keys.UP) {
+                        yDirection = -1;
+                    } else if (event == Keys.DOWN) {
+                        yDirection = 1;
+                    }
+
+                    playerLocation.y += yDirection;
+
+                    if (playerLocation.y < 0) {
+                        playerLocation.y = 0;
+                    } else if (playerLocation.y > HEIGHT - Greta.playerHeight) {
+                        playerLocation.y = HEIGHT - Greta.playerHeight;
+                    }
+
+                    return playerLocation;
+                })
+                .throttleFirst(35, TimeUnit.MILLISECONDS)
+                .observeOn(gameEngine.getRenderThread())
+                .subscribe(gameEngine::drawSinglePlayer);
     }
 
     public static void main(String[] args) {
